45baf9213b79604a4cb0e585f21a8dbc66f59575
fecke.smile@hotmail.it <fecke.smile@hotmail.it>
2020-01-13 16:28:18 +0100

    finished exercise

--- exercises/scoring_blosum50.py
+++ exercises/scoring_blosum50.py
@@ -8,30 +8,28 @@ def score_blosum50():
 	seq1=input("seq1: ")
 	seq2=input("seq2: ")
 	score=0
-	blosum50=open("../data/blosum.txt","r")
+	blosum50=open("../data/blosum.txt","r") 
+
 	if len(seq1)!=len(seq2):
 		print("le due sequenze non hanno la stessa lunghezza")
 		blosum50.close()
 		score_blosum50()
 	else:
-		blosumdict={}
-		blosum=[]
-		aa=blosum50.readline()
-		aa=aa.split()
-		for line in blosum50:
-			line=line.rstrip()
-			line=line[2:].split()
-			line=int_list(line)
-			blosum.append(line)
-					
-		for res in aa:
-			for res2 in aa:
-				blosumdict[res+res2]=0
-
+		blosum=[] 			#dichiara una variabile lista 
+		for line in blosum50:		
+			line=line.rstrip()	
+			line=line.split()	
+			#print(line)
+			blosum.append(line)	#blosum è una lista di liste (tabella)
+			
+		blosumdict={}			#dichiara un dizionario vuoto
+		for i in range(len(blosum[0])):	#for loop necessario per la prima iterazione nella tabella // va da o alla lunghezza della lista in posizione 0 di lista
+			for j in range(1, len(blosum)): #for loop che va da 1 a lunghezza di blosum
+				blosumdict[blosum[0][i]+blosum[j][0]]=blosum[i+1][j] #crea una key che è data dalla somma del valore della prima riga + il valore della prima colonna e il value associato è il valore del valore
 		
-				
-	return blosumdict
+		for base1,base2 in zip(seq1,seq2):
+			score+=int(blosumdict[base1+base2])
+					
+	return score
 
 print(score_blosum50())
-
-

e17b6bc481ba1a369314eef91f76ffc4df94e0e7
fecke.smile@hotmail.it <fecke.smile@hotmail.it>
2020-01-15 21:55:34 +0100

    slightly changes to make it work for diagonal matrices

--- exercises/store_diag_mx.py
+++ exercises/store_diag_mx.py
@@ -28,21 +28,18 @@ def scoring_dic():
     sc_dict = {}
     for aa1 in aa:
         for aa2 in aa:
-            print(aa1,aa2)
-            if sc_dict[aa2+aa1] not in sc_dict:
+            if (aa1+aa2 not in sc_dict) and (aa2+aa1 not in sc_dict):
                 sc_dict[aa1+aa2] = 0
-            print(aa1+aa2)
 
-    print(sc_dict)
 
-    # i = 0
-    # while i < len(sc_dict):  # fin tanto che i è minore di len(sc_dict) in questo caos 400
-    #     line = sc_mx_file.readline()
-    #     line = line.split()
-    #     line = line[1:]
-    #     for val in line:
-    #         sc_dict[get_nth_key(sc_dict, i)] = float(val)
-    #         i += 1
+    i = 0
+    while i < len(sc_dict):  # fin tanto che i è minore di len(sc_dict) in questo caos 400
+        line = sc_mx_file.readline()
+        line = line.split()
+        line = line
+        for val in line:
+            sc_dict[get_nth_key(sc_dict, i)] = float(val)
+            i += 1
     return sc_dict
 
 

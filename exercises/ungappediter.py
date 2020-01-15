054284b60677d450edd90ce3221dc8334ef49f7e
fecke.smile@hotmail.it <fecke.smile@hotmail.it>
2020-01-15 11:59:47 +0100

    comments added

--- exercises/ungappediter.py
+++ exercises/ungappediter.py
@@ -1,7 +1,26 @@
 ##################################
-#   Ungapped iterative version   #
+#   Ungapped iterative version
 ##################################
-
+# 2) Write a script that generates all the possible ungapped alignments of two sequences, scores them and identifies
+# the best scoring ones.
+#
+# These are all the possible ungapped alingments of the two sequences: TCA and GA:
+#
+# --TCA  -TCA  TCA  TCA  TCA-  TCA--
+# GA---  GA--  GA-  -GA  --GA  ---GA
+#
+# Using the following scoring scheme:
+#
+# score_matrix = {'AA': 2, 'AC':-1, 'AT':-1, 'AG': 0,
+#                 'CA':-1, 'CC': 2, 'CT': 0, 'CG':-1,
+#                 'TA':-1, 'TC': 0, 'TT': 2, 'TG':-1,
+#                 'GA': 0, 'GC':-1, 'GT':-1, 'GG': 2}
+#
+#
+# The best scoring alignment is:
+# TCA
+# -GA
+#######################################################################################################################
 
 def score_ali(seq1, seq2, score_mtrx):
     ''' Function that computes the score between two sequences. Input values are: sequence one, sequence two, and the scoring matrix chosen '''
